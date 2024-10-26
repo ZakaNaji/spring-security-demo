37697a28151605f2463346f317708ba1350e690f
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-26 13:32:23 +0100

    #Spring security demo: UserDetailsService - this bean helps spring security to load users based on there username, - interface has multiple impl, dep on how/where we want to retrieve our users. - in this example we use InMemoryUserDetailsManager, this implimentation takes a list of users and keeps them in a map in memory, when spring security provides a username, it will try to find the user in one of it entries.

--- src/main/java/com/znaji/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/znaji/springsecuritydemo/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,6 +22,21 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
                 .httpBasic(withDefaults())
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}1234")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1, admin);
     }
 }
