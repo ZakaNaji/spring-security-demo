fdbf624c5b7fadb817a8039e55fe91bcb0463cd3
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-27 19:15:15 +0100

    #Spring security demo: JwtAuthFilter - this custom filter will be added to our SecurityConfig in the filter chain - will be used to parse the token from the header of the resquest and find if used is auth

---
+++ src/main/java/com/znaji/springsecuritydemo/jwt/JwtAuthFilter.java
@@ -0,0 +1,54 @@
+package com.znaji.springsecuritydemo.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private  JwtUtils jwtUtils;
+    private UserDetailsService userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            final String jwt = parseJwtToken(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                final String username = jwtUtils.getUsernameFromJwtToken(jwt);
+                final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                final UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwtToken(HttpServletRequest request) {
+        return jwtUtils.getJwtSecretFromHeader(request);
+    }
+
+    @Autowired
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Autowired
+    public void setJwtUtils(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+}
